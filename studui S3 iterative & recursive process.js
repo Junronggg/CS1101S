6502368f063946520bde6a3baba5bf64f9607312
Source Academy User <No public email provided>
2024-08-26 18:03:57 +0800

    Changes made from Source Academy

--- studui S3 iterative & recursive process.js
+++ studui S3 iterative & recursive process.js
@@ -1,4 +1,4 @@
-import {square, heart, show, stack, beside, blank} from "rune";
+import {square, heart, show, stack, beside, blank, circle, stack_frac, beside_frac} from "rune";
 
 //iterative
 function f1(rune1, n, rune2)
@@ -35,3 +35,44 @@ function f21(rune, n)
 show(f2(heart, 3));
 show(f21(heart, 3));
 
+
+function moony_1(bottom_right)
+{
+    return stack(beside(circle, blank), beside(square, bottom_right));
+}
+show(moony_1(circle));
+
+function moony_2(n)
+{
+    return n===2
+           ? moony_1(circle)
+           : moony_1(moony_2(n-1));
+}
+show(moony_2(5));
+
+function moony1(n)
+{ return n===1
+           ? circle
+           : beside(stack_frac(1/n, circle, square), stack_frac(1/n, blank, moony1(n-1)));
+}
+
+//recursive
+function moony(n)
+{
+    return n===1
+           ? circle
+           : beside_frac(1/n, stack_frac(1/n, circle, square), stack_frac(1/n, blank, moony(n-1)));
+}
+show(moony1(5));
+show(moony(5));
+
+//iterative
+function moony11(n, counter, shape)
+{
+    return counter>n
+           ? shape
+           : moony11(n-1, 
+                    counter+1, 
+                    beside_frac(1/(counter+1), stack_frac(1/(counter+1), circle, square), stack_frac(1/(counter+1), blank, shape)));
+}
+show(moony11(3, 1, circle));
