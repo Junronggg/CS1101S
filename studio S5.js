c888b016804656df2bc43152f7e3d2ee49a3b848
Source Academy User <No public email provided>
2024-09-09 13:02:39 +0800

    Changes made from Source Academy

---
+++ studio S5.js
@@ -0,0 +1,25 @@
+draw_data(list(list(1, 2, list(3)), list(4, 5), pair(6, 7)));
+draw_data(pair(1, list(2, 3, pair(4, null))));
+draw_data(pair(1, pair(2, list(3, list(4, 5)))));
+display_list(list(list(1, 2, list(3)), list(4, 5), pair(6, 7)));
+
+
+const lst1=list(7, list(6, 5, 4), 3, list(2, 1));
+const lst2=list(list(7), list(6, 5, 4), list(3, 2), 1);
+const lst3=list(7, list(6), list(5, list(4)), list(3, list(2, list(1))));
+const lst4=list(7, list(list(6, 5), list(4), 3, 2), list(list(1)));
+
+head(tail(head(tail(tail(tail(lst1))))));
+head(tail(tail(tail(lst2))));
+head(head(tail(head(tail(head(tail(tail(tail(lst3)))))))));
+head(head(head(tail(tail(lst4)))));
+
+const lst=list("a", "x", "b", "y", "c", "z", "d");
+function every_second(list)
+{
+    return is_null(list) || length(list)<=1
+           ? null
+           : pair(head(tail(list)), 
+                  every_second(tail(tail(list))));
+}
+every_second(lst);
