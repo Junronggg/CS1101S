e9a805d108fde7c4bc63c693e48cdab7ae0e6c31
Source Academy User <No public email provided>
2024-10-17 11:26:16 +0800

    Changes made from Source Academy

---
+++ L8B CSE machine.js
@@ -0,0 +1,40 @@
+//exp
+//1 - 5 / (2 * 4) + 3;
+
+//declaration
+// const x=1;
+
+//sequence
+// 2+3;
+// 4/5;
+// 6*7;
+
+//conditional exp
+// const x = true;
+// x ? 1 + 2 : 3 * 4;
+
+//logical composition
+// const x = true;
+// const y = false;
+// x && y;
+
+//conditional statement
+// let x = 1;
+// if (x > 2) {
+//     11;
+//     } else {
+//         22;
+//     }
+
+//while loop (fac eg)
+let n = 5;
+let i = 1;
+let result = 1;
+while (i < n) {
+    i = i + 1;
+    result = result * i;
+}
+
+
+
+
